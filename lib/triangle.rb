e22df4776408e929ef2450d22bf548c55b9df878
Shamel Lakin <christlife4u@gmail.com>
2019-10-16 12:30:30 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,42 +1,32 @@
 class Triangle
   
-#   def initialize(side_1,side_2,side_3)
-#     @side_1 = side_1
-#     @side_2 = side_2
-#     @side_3 = side_3
-#   end
+  def initialize(side_1,side_2,side_3)
+    @side_1 = side_1
+    @side_2 = side_2
+    @side_3 = side_3
+  end
   
-#   def kind()
-#     if (@side_1 <= 0) || (@side_2 <= 0) || (@side_3 <= 0)
-#       raise TriangleError
-#     elsif (@side_1+@side_2 <= @side_3) || (@side_1+@side_3 <= @side_2) || (@side_2+@side_3 <= @side_1)
-#       raise TriangleError
-#     else
-#       if (@side_1 == @side_2) && (@side_2 == @side_3)
-#         :equilateral
-#       elsif (@side_1 == @side_2) || (@side_2 == @side_3) || (@side_1 == @side_3)
-#         :isosceles
-#       elsif (@side_1 != @side_2) && (@side_2 != @side_3) && (@side_1 != @side_3)
-#         :scalene
-#       #   else (@side_1 != 0) || (@side_2 != 0) || (@side_3 != 0)
-#       # raise TriangleError
-#       end
-#     end
+  def kind()
+    if (@side_1 <= 0) || (@side_2 <= 0) || (@side_3 <= 0)
+      raise TriangleError
+    elsif (@side_1+@side_2 <= @side_3) || (@side_1+@side_3 <= @side_2) || (@side_2+@side_3 <= @side_1)
+      raise TriangleError
+    else
+      if (@side_1 == @side_2) && (@side_2 == @side_3)
+        :equilateral
+      elsif (@side_1 == @side_2) || (@side_2 == @side_3) || (@side_1 == @side_3)
+        :isosceles
+      elsif (@side_1 != @side_2) && (@side_2 != @side_3) && (@side_1 != @side_3)
+        :scalene
+      #   else (@side_1 != 0) || (@side_2 != 0) || (@side_3 != 0)
+      # raise TriangleError
+      end
+    end
 
-#   end
+  end
   
-# end
-
-
-def kind(a, b, c)
-  a, b, c = [a, b, c].sort
-  if a <= 0 or a + b <= c
-  raise TriangleError
-   :equilateral if a == c
-   :isosceles if a == b or b == c
-   :scalene
 end
-end 
+
 class TriangleError < StandardError
   def message
     "This is an illegal triangle"
