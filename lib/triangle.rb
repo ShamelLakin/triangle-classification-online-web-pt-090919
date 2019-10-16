a4e468845ff84c739e71a3ac9134860fac9558d2
Shamel Lakin <christlife4u@gmail.com>
2019-10-16 12:59:29 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -18,7 +18,7 @@ class Triangle
         :isosceles
       elsif (@side_1 != @side_2) && (@side_2 != @side_3) && (@side_1 != @side_3)
         :scalene
-      elsif (@side_1 == 0) && (@side_2 == 0) && (@side_3 == 0)
+      elsif (@side_1 == false) && (@side_2 == false) && (@side_3 == false)
       raise TriangleError
       end
     end
