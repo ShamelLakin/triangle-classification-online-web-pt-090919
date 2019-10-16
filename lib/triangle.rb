9ecb89ebe41ae8b8f4530d9cd8390a959570c706
Shamel Lakin <christlife4u@gmail.com>
2019-10-16 13:00:49 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -11,7 +11,7 @@ class Triangle
       raise TriangleError
     elsif (@side_1+@side_2 <= @side_3) || (@side_1+@side_3 <= @side_2) || (@side_2+@side_3 <= @side_1)
       raise TriangleError
-      elsif (@side_1 == false) && (@side_2 == false) && (@side_3 == false)
+      elsif (@side_1 == 0) && (@side_2 == 0) && (@side_3 == 0)
       raise TriangleError
     else
       if (@side_1 == @side_2) && (@side_2 == @side_3)
