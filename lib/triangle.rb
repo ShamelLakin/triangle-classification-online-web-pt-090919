08bf4722a9ef5bde70eddd1cd5cea3911674dca0
Shamel Lakin <christlife4u@gmail.com>
2019-10-16 13:16:47 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,41 @@
 class Triangle
-  # write code here
+  
+  def initialize(side_1,side_2,side_3)
+    @side_1 = side_1
+    @side_2 = side_2
+    @side_3 = side_3
+  end
+  
+  def kind()
+    if (@side_1 <= 0) || (@side_2 <= 0) || (@side_3 <= 0)
+      raise TriangleError
+    elsif (@side_1+@side_2 <= @side_3) || (@side_1+@side_3 <= @side_2) || (@side_2+@side_3 <= @side_1)
+      raise TriangleError
+    else
+      if (@side_1 == @side_2) && (@side_2 == @side_3)
+        :equilateral
+      elsif (@side_1 == @side_2) || (@side_2 == @side_3) || (@side_1 == @side_3)
+        :isosceles
+      elsif (@side_1 != @side_2) && (@side_2 != @side_3) && (@side_1 != @side_3)
+        :scalene
+      elsif (@side_1 == false) && (@side_2 == false) && (@side_3 == false)
+      raise TriangleError
+      end
+    end
+
+  end
+  
+  class TriangleError < StandardError
+  def message
+    "This is an illegal triangle"
+  end
 end
+  
+end
+
+# class TriangleError < StandardError
+#   def message
+#     "This is an illegal triangle"
+#   end
+# end
+
